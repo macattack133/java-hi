e45a25cea491a79e2071ffba4d05e6eaacf96d33
mcalhoun133 <mcalhoun133@gmail.com>
2024-04-23 11:11:59 -0700

    Starting repository

---
+++ src/main/java/org/dirtypucks/javahi/JavaHiApplication.java
@@ -0,0 +1,13 @@
+package org.dirtypucks.javahi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaHiApplication.class, args);
+	}
+
+}
